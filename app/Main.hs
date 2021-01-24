8182dda3dcea25e155678a8f677014d8b9c99358
Marek Dudek <marek.dudek@gmail.com>
2021-01-24 18:52:59 +0100

    examples from docs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Lib
+import Person
+import Data.YAML
+
+import qualified Data.ByteString.Lazy.Char8 as C
+
 
 main :: IO ()
-main = someFunc
+main = do
+  let Right ps = decode "- name: Erik Weisz\n  age: 52\n  magic: True\n- name: Mina Crandon\n  age: 53" :: Either (Pos,String) [[Person]]
+  print (ps :: [[Person]])
+  let Right is = decode1 "- 1\n- 2\n- 3\n" :: Either (Pos,String) [Int]
+  print (is :: [Int])
+  let bs = encode [Person {name = "Vijay", age = 19, magic = False}]
+  C.writeFile "out/person.yaml" bs
---
+++ src/Person.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Person where
+
+import Data.YAML
+import Data.Text
+
+data Person = Person
+    { name  :: Text
+    , age   :: Int
+    , magic :: Bool
+    } deriving Show
+
+
+instance FromYAML Person where
+   parseYAML = withMap "Person" $ \m -> Person
+       <$> m .: "name"
+       <*> m .: "age"
+       <*> m .:? "magic" .!= False
+
+instance ToYAML Person where
+    toYAML (Person n a m) = mapping [ "name" .= n, "age" .= a, "magic" .= m]
