ce0c0d3b9a6ee7106952845ed090feb3ff939ed7
Marek Dudek <marek.dudek@gmail.com>
2021-01-24 19:55:32 +0100

    decoding tests

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,5 @@ import qualified Data.ByteString.Lazy.Char8 as C
 
 main :: IO ()
 main = do
-  let Right ps = decode "- name: Erik Weisz\n  age: 52\n  magic: True\n- name: Mina Crandon\n  age: 53" :: Either (Pos,String) [[Person]]
-  print (ps :: [[Person]])
-  let Right is = decode1 "- 1\n- 2\n- 3\n" :: Either (Pos,String) [Int]
-  print (is :: [Int])
   let bs = encode [Person {name = "Vijay", age = 19, magic = False}]
   C.writeFile "out/person.yaml" bs
--- test/PersonSpec.hs
+++ test/PersonSpec.hs
@@ -10,9 +10,23 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
-    describe "Person" $
-        it "can be decoded from string" $
-          let Right ps = decode "- name: Erik Weisz\n  age: 52\n  magic: True\n- name: Mina Crandon\n  age: 53" :: Either (Pos,String) [[Person]]
-          in
-            ps `shouldBe` [[Person {name = "Erik Weisz", age = 52, magic = True}, Person {name = "Mina Crandon", age = 53, magic = False}]]
+spec = do
+    describe "Person" $ do
+        let erik =
+              Person {
+                name = "Erik Weisz" ,
+                age = 52,
+                magic = True
+              }
+            mina =
+              Person {
+                name = "Mina Crandon",
+                age = 53,
+                magic = False
+              }
+        it "can decode multiple persons from string" $ do
+          let Right [ps] = decode "- name: Erik Weisz\n  age: 52\n  magic: True\n- name: Mina Crandon\n  age: 53" :: Either (Pos,String) [[Person]]
+          ps `shouldBe` [erik, mina]
+        it "can decode single person from string" $ do
+          let Right [p] = decode1 "- name: Erik Weisz\n  age: 52\n  magic: True" :: Either (Pos,String) [Person]
+          p `shouldBe` erik
